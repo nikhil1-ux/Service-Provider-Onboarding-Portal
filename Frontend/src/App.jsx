97350657144afcf55f5f350f92fa715f5bd95e91
nikhil1-ux <nikhilyadav9044@gmail.com>
2026-07-31 04:27:34 +0530

    fix: correct import paths for AuthProvider and AppRoutes in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import { AuthProvider } from "./context/AuthContext";
-import AppRoutes from "./routes/routes";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import AppRoutes from "./routes/Routes.jsx";
 
 
 function App() {
