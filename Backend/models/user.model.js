5fd9475db9fe082b4e612ed5b1fc5184b3463271
nikhil1-ux <nikhilyadav9044@gmail.com>
2026-07-31 05:43:05 +0530

    fix: remove unnecessary next() call in password hashing middleware

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -67,7 +67,7 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
-  next();
+  
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
